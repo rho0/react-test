afe8b1827803abd6eb3ba92721e4a07e69cd879d
rho <lgnsy123@gmail.com>
2024-06-11 17:28:25 +0900

    7_tictactoe end, 8_minesearch start

--- 8_MineSearch/Form.jsx
+++ 8_MineSearch/Form.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Form = () => {
-  return
+  const [row, setRow] = useState(10);
+  const [cell, setCell] = useState(10);
+  const [mine, setMine] = useState(20);
+
+  return (
+    <div>
+      <input type="number" placeholder="세로" value={row} onChange={onChangeRow} />
+      <input type="number" placeholder="가로" value={row} onChange={onChangeRow} />
+      <input type="number" placeholder="지뢰" value={row} onChange={onChangeRow} />
+    </div>
+  )
 
 }
 
